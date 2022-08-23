a0e00b86ca46541d0837720c87a0a117a82a6245
Kolo <konatealim@gmail.com>
2022-08-23 15:39:23 -0700

    add

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                to_p = "FizzBuzz"
+            else:
+                to_p = "Fizz"
+        elif i % 5 == 0:
+            to_p = "Buzz"
+        else:
+            to_p = str(i)
+        print("{:s}".format(to_p), end = " ")
