0856159024736fea76b9c6b219f107718b4f46aa
Kolo <konatealim@gmail.com>
2022-08-25 13:07:20 -0700

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 from sys import argv
 if __name__ == "__main__":
     s = 0
-    for i in range(1 ,len(argv)):
+    for i in range(1, len(argv)):
         s += int(argv[i])
     print("{:d}".format(s))
