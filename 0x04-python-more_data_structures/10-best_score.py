fa45c80ac271662e9ec30eb6203837af577e7379
Kolo <konatealim@gmail.com>
2022-08-31 17:34:44 -0700

    add

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary == None or a_dictionary == {}:
         return None
-    maxi = max(a_dictionay.values())
+    maxi = max(a_dictionary.values())
     for i in a_dictionary.keys():
         if a_dictionary[i] == maxi:
             return maxi
