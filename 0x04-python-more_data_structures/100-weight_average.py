e62150af631b700943b117c4f0da71c61b6f246d
Kolo <konatealim@gmail.com>
2022-08-31 17:57:42 -0700

    add

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list == []:
+        return 0i
+    som = 0
+    coe = 0
+
+    for i, j in my_list:
+        som += i * j
+        coe += j
+    return som / coe
+
