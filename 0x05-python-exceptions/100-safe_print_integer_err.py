0b0d7404a49d9493ce9055097ca2c86cb97b5519
Kolo <konatealim@gmail.com>
2022-09-12 06:53:08 -0700

    add

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    boo = True
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        boo = False
+        sys.stderr.write("Exception: {}\n".format(err))
+    except TypeError:
+        boo = False
+        sys.stderr.write("Exception: {}\n".format(err))
+    return boo
