917fd47cc7947be3b1c5bdf8da9ac173c86b980e
Kolo <konatealim@gmail.com>
2022-09-13 07:04:36 -0700

    add

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,7 +3,11 @@
 
 
 class Square:
-    """A Square class with"""
+    """Class Square that defines a square
+    
+    Attributes:
+    __size (int): size of a size of the square
+    __position (tuple): position of the square in 2D"""
     def __init__(self, size=0, position=(0, 0):
         self.__size = size
         self.__position = position
