4166d6817b4bf317e1572f39b094c360b65b1277
Kolo <konatealim@gmail.com>
2022-09-13 16:50:27 -0700

    add

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"Defines a class Square"
+from math import pi
+
+
+class MagicClass:
+    """A Magic class"""
+    def __init__(self, radius=0):
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return pi * self.__radius ** 2
+
+    def circumference(self):
+        return 2 * pi * self.__radius
