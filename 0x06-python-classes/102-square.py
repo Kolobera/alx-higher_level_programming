8fc819518a85f88258eb209baf3ba7c8fbc6d746
Kolo <konatealim@gmail.com>
2022-09-13 16:38:31 -0700

    add

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"Defines a class Square"
+
+
+class Square:
+    """Class Square that defines a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return(self.__size**2)
+
+    def __eq__(self, o):
+        return self.area() == o.area()
+
+    def __lt__(self, o):
+        return self.area() < o.area()
+
+    def __le__(self, o):
+        return self.area() <= o.area()
+
+    def __ne__(self, o):
+        return self.area() != o.area()
+
+    def __gt__(self, o):
+        return self.area() > o.area()
+
+    def __ge__(self, o):
+        return self.area() >= o.area()
