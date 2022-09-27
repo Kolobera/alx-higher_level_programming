0114bbfedba194cf95e6b9bb6a55fe0a6f6742c0
Ousmane Konate <konatealim@gmail.com>
2022-09-27 03:13:17 +0200

    add

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The my int module"""
+
+
+class MyInt(int):
+    """Myint class"""
+    def __eq__(self, x):
+        return super().__ne__(x)
+    def __ne__(self, x):
+        return super().__eq__(x)
