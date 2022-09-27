a557adbeadc5563c004b4c4dda55e2ee5093dd55
Ousmane Konate <konatealim@gmail.com>
2022-09-27 18:27:14 +0200

    add

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,11 +10,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        lp = {}
-        if attrs is not None and attrs != []:
-            for i in attrs:
-                if i in self.__dict__.keys():
-                    lp[i] = self.__dict__[i]
-        else:
-            lp = self.__dict__
-        return lp
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
