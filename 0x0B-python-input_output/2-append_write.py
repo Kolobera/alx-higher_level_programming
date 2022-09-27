4a09c93dfab945ed35badab468d3f3fe8d353354
Ousmane Konate <konatealim@gmail.com>
2022-09-27 16:22:07 +0200

    add

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """The append write file module"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Append text into file"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
