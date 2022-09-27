a8c7f4b395eefa6a8f62c827faec072e3dafb4b3
Ousmane Konate <konatealim@gmail.com>
2022-09-27 16:14:02 +0200

    add

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,13 @@
 
 def write_file(filename="", text=""):
     """Write text into file"""
+    ln = 0
+    for i in text:
+        if i == "\n":
+            ln += 5
     with open(filename, "w") as file:
         file.write(text)
-    return len(text)
+    return len(text) + ln
 
-print(write_file("lk.txt", "oijdoj jeniejr\n ejdojze zieiz"))
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
