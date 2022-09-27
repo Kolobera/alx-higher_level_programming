0b8b47026181e5de5c1ad7841aec1bd9bd470bac
Ousmane Konate <konatealim@gmail.com>
2022-09-27 17:18:46 +0200

    add

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""The to json string module"""
+import json
+from sys import argv
+save_to_json_file =__import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from os.path import exists
+
+
+lp = []
+if exists("add_item.json"):
+    with open ("add_item.json", "r", encoding="Utf-8") as file:
+        lines = file.readlines()
+        for i in lines:
+            if i != "":
+                lp = json.loads(i)
+else:
+    with open ("add_item.json", "w", encoding="Utf-8") as file:
+        pass
+
+
+
+with open ("add_item.json", "r", encoding="Utf-8") as file:
+    lines = file.readlines()
+    for i in lines:
+        if i != "":
+            lp = json.loads(i)
+
+for i in range(1, len(argv)):
+        lp.append(argv[i])
+print(lp)
+with open("add_item.json", "w", encoding="Utf-8") as file:
+    lpo = "[" + ", ".join(lp) + "]"
+    file.write(json.dumps(lp))
