cd820c1ad2084bcf55186b98c2e1f0059c3b57c1
Ousmane Konate <konatealim@gmail.com>
2022-09-27 16:43:09 +0200

    add

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The to json string module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save json representation of
+    my_obj in filename"""
+    with open(filename, "w", encoding=" Utf-8") as file:
+        file.write(json.dumps(my_obj))
