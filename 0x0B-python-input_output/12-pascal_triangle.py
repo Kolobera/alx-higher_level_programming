eb248e039b1edebb25f95b04dec8807e8293e91a
Ousmane Konate <konatealim@gmail.com>
2022-09-28 01:43:04 +0200

    add

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,8 +11,8 @@ def pascal_triangle(n):
         if i == 0:
             lp.append([1])
         else:
-            x = [0] + lp[-1] + [0]
-            lm = [x[l] + x[l+1] for l in range(len(x) - 1)]
+            j = [0] + lp[-1] + [0]
+            lm = [j[el] + j[el+1] for el in range(len(j) - 1)]
             lp.append(lm)
 
     return lp
