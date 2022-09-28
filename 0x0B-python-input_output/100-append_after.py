f28e29ba2602d30fac67b785e76c7cc7ae66ffef
Ousmane Konate <konatealim@gmail.com>
2022-09-28 02:00:32 +0200

    add

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The Append after module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append new string after search string"""
+    with open(filename, "r") as file:
+        data = file.readlines()
+    lp = []
+    for i in range(len(data)):
+        lp.append(data[i])
+        if search_string in data[i]:
+            lp.append(new_string)
+    
+    with open(filename, "w") as file:
+        file.write("".join(lp))
