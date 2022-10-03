fb0a8c7be81a89c92d2c99cde1a8c43375346a07
Ousmane Konate <konatealim@gmail.com>
2022-10-03 20:00:45 +0200

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -64,7 +64,7 @@ class Base:
         with open(file_load, "r", encoding="utf-8")\
                 as file:
             return[cls.create(**dic) for dic in
-                    cls.from_json_string(file.read())]
+                cls.from_json_string(file.read())]
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
