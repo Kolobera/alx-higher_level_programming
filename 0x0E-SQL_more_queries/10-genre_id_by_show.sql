8d289eb2aa73f3980a0a1908b45687f3289a88f7
Kolo <konatealim@gmail.com>
2022-11-02 11:56:44 -0700

    add

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
- s.`title`, g.`genre_id`
+SELECT s.`title`, g.`genre_id`
 FROM `tv_shows` as s 
 INNER JOIN `tv_show_genres` as g
 ON s.`id` = g.`show_id`
