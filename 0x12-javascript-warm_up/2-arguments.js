0f5e7a9dee305fae73631cbb08ebcd588ef8d5cf
Kolo <konatealim@gmail.com>
2022-11-28 13:46:08 -0800

    add

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const i = process.argv.length;
+if (i == 3)
+{
+	console.log('Argument found');
+}
+else if (i > 3)
+{
+	console.log('Arguments found');
+}
+else
+{
+	console.log('No argument');
+}
