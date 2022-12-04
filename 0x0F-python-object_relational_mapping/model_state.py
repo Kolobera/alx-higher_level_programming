398a72ac667add5539cf8e80f5141be8f0b99341
Kolo <konatealim@gmail.com>
2022-12-04 03:13:27 -0800

    add

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
