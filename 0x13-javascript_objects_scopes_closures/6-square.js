933c973eea991b917506cddfb320ac405940bb1e
Ousmane Konate <konatealim@gmail.com>
2022-12-11 13:07:31 +0100

    add

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    if (!c) {
+        super.print();
+    }
+    else {
+        for (let i = 0; i < this.size; i++) {
+            console.log(c.repeat(this.size));
+        }
+    }
+  }
+};
