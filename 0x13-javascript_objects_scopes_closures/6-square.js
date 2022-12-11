dde2b0a403c63dac8faaa7694c5d9cab88c9d7f0
Ousmane Konate <konatealim@gmail.com>
2022-12-11 13:09:28 +0100

    add

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,7 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-  charPrint(c) {
+  charPrint (c) {
     const char = c || 'X';
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
