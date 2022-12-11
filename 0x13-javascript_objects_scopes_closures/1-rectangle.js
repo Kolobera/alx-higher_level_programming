c923673396fff27a1fd8eba189ea53928df021b3
Ousmane Konate <konatealim@gmail.com>
2022-12-11 12:05:37 +0100

    add

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-    width;
-    height;
-
-    constructor(w, h) {
-        this.width = w;
-        this.height = h;
+module.exports = class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
     }
-}
+  };
