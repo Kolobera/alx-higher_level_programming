dab8f2656be3d1ca4ed6376e812bbf0f4c7647f6
Kolo <konatealim@gmail.com>
2022-12-26 05:56:07 -0800

    add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,9 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     re = f"https://api.github.com/repos/{argv[1]}/{argv[2]}/commits"
     r = requests.get(re)
-    lp = list(r.json())
-    i = 0
-    while i < 10:
-        lm = dict(lp[i])
-        print(f"{lm['sha']}: {lm['commit']['author']['name']}")
-        i += 1
+    commits = r.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+            commit.get("commit").get("author").get("name")))
