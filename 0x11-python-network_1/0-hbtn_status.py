7b11eb321c480f77b3450967e3e9855222b39d5b
Kolo <konatealim@gmail.com>
2022-12-26 03:03:25 -0800

    add

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ import urllib.request
 
 def myStatus():
     """what's my status"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
