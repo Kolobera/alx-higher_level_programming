9e7c6ad9e0b66e5b5d6288cf4f54f59f5da9c091
Kolo <konatealim@gmail.com>
2022-12-26 03:13:27 -0800

    add

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,13 @@
 import urllib.request
 import sys
 
+
 def myStatus():
     """what's my status"""
     request = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(request) as response:
         print("{}".format(response.getheader("X-Request-Id")))
+
 
 if __name__ == "__main__":
     myStatus()
