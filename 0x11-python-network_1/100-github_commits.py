f53de5ff21158441f9f4e207925c883e88f42668
Kolo <konatealim@gmail.com>
2022-12-26 06:53:28 -0800

    add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,13 +5,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    re = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits?q=&per_page=100"
+    re = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     r = requests.get(re)
     commits = r.json()
     i = 0
     for commit in commits:
         if i < 10:
-            #if "BEGIN" in str(commit.get("commit").get("verification").get("signature")):
-                print("{}: {}".format(commit.get("sha"),
-                      commit.get("commit").get("author").get("name")))
-                i += 1
+            print("{}: {}".format(commit.get("sha"),
+                  commit.get("commit").get("author").get("name")))
+            i += 1
