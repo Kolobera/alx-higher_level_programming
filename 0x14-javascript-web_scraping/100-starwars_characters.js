b3eefbce97e50225eb544ceaf01a90e19738697e
Kolo <konatealim@gmail.com>
2023-01-17 20:30:54 +0000

    add

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 const fs = require('request');
+const fcx = require('request');
 
-fs('https://swapi-api.alx-tools.com/api/films/' + process.argv.slice(2)[0], function (error, body) {
+fs('https://swapi-api.alx-tools.com/api/films/' + process.argv.slice(2)[0], function (error, response, body) {
     if (error) {
         console.log(error);
       }
-  for (const wars of JSON.parse(body).results) {
-    console.log(wars.characters);
+  for (const wars of JSON.parse(body).characters) {
+    fcx(wars, function (error, response, body){
+	if (error) {
+	console.log(error);
+	}
+	console.log(JSON.parse(body).name);
+	});
     }
 });
