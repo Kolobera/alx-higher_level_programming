2e741ea2ac49175f55000ce4c668ccdca1220d32
Ousmane Konate <konatealim@gmail.com>
2023-01-17 21:17:56 +0100

    add

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,9 +2,10 @@
 const fs = require('request');
 
 fs('https://swapi-api.alx-tools.com/api/films/' + process.argv.slice(2)[0], function (error, body) {
-  if (JSON.parse(body.body).title !== undefined) {
-    console.log(JSON.parse(body.body).title);
-    return;
-  }
-  console.log(error);
+    if (error) {
+        console.log(error);
+      }
+  for (const wars of JSON.parse(body).results) {
+    console.log(wars.characters);
+    }
 });
